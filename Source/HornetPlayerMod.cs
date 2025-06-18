c37c7cb94a8380f57e9e891a0ca5d9a5e2963163
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-06-18 09:33:57 +0200

    Hollow Knight Example Mod

---
+++ Source/HornetPlayerMod.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Modding;
+
+namespace HornetPlayer;
+
+public class HornetPlayerMod : Mod, ITogglableMod {
+    public static HornetPlayerMod? LoadedInstance { get; private set; }
+
+    public override string GetVersion() {
+        return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+
+    public override void Initialize() {
+        if (LoadedInstance != null) return;
+        LoadedInstance = this;
+    }
+
+    public void Unload() {
+        LoadedInstance = null;
+    }
+}
